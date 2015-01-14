85995d1a12392b34d77b9958e3e0063505fc1a1d
Fadie Hannona <fadiehannona@gmail.com>
2015-01-14 16:42:47 +0000

    omniauth fb provider now working

--- insta_rails/config/routes.rb
+++ insta_rails/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :only => :new_user_session 
+    get 'sign_out', :to => 'devise/sessions#destroy', :only => :destroy_user_session
+  end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root to: "posts#index"
 
